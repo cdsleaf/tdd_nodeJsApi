d81e94ee56cf62381bf9719fdc2a5061c1bc6519
daesang.cho <daesang.cho@samsung.com>
2018-02-26 16:37:50 +0900

    add sqlite

--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -1,29 +1,32 @@
 const request = require('supertest');
 const should = require('should');
 const app = require('../../index');
+const models = require('../../models');
 
-describe('GET /users는', ()=> {
+describe.only('GET /users는', ()=> {
     describe('성공시', ()=>{
-        it('유저 객체를 담은 배열로 응답한', (done)=>{
+        const users = [{name: 'alice'}, {name: 'bek'}, {name: 'chris'}]
+        before(()=>models.sequelize.sync({force:true}));
+        before(()=>models.User.bulkCreate(users));
+        it('유저 객체를 담은 배열로 응답한', ()=>{
             request(app)
                 .get('/users')
                 .end((err, res)=>{
                     res.body.should.be.instanceOf(Array);
                     done();
-                })
-        })
-    });
-
-    it('최대 limit 갯수만큼 응답한다.', (done) => {
-        request(app)
-            .get('/users?limit=2')
-            .end((err, res)=>{
-                res.body.should.have.lengthOf(2);
-                done();
-            });
+                });
+        });
+        it('최대 limit 갯수만큼 응답한다.', ()=>{
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res)=>{
+                    res.body.should.have.lengthOf(2);
+                    done();
+                });
+        });
     });
     describe('실패시', ()=>{
-        it('limit이 숫자형이 아니면 400을 응답한다.', (done) => {
+        it('limit이 숫자형이 아니면 400을 응답한다.', (done)=>{
             request(app)
                 .get('/users?limit=two')
                 .expect(400)
@@ -34,7 +37,7 @@ describe('GET /users는', ()=> {
 
 describe('GET /users/1는', ()=>{
     describe('성공시', ()=>{
-        it('id가 1인 유저 객체를 반환한다.', ()=>{
+        it('id가 1인 유저 객체를 반환한다.', done=>{
             request(app)
                 .get('/users/1')
                 .end((err, res) =>{
@@ -44,13 +47,13 @@ describe('GET /users/1는', ()=>{
         })
     })
     describe('실패시', ()=>{
-        it('id가 숫자가 아닐경우 400으로 응답한다.', (done)=>{
+        it('id가 숫자가 아닐경우 400으로 응답한다.', done=>{
             request(app)
                 .get('/users/one')
                 .expect(400)
                 .end(done);
         });
-        it('id로 유저를 찾을수 없을 경우 404로 응답한다.', (done)=>{
+        it('id로 유저를 찾을수 없을 경우 404로 응답한다.', done=>{
             request(app)
                 .get('/users/999')
                 .expect(404)
@@ -61,7 +64,7 @@ describe('GET /users/1는', ()=>{
 
 describe('DELETE /users/:id', ()=>{
     describe('성공시', ()=>{
-        it('204를 응답한다', (done)=>{
+        it('204를 응답한다', done=>{
             request(app)
                 .delete('/users/1')
                 .expect(204)
@@ -69,7 +72,7 @@ describe('DELETE /users/:id', ()=>{
         })
     });
     describe('실패시', ()=>{
-        it('id가 숫자가 아닐경우 400으로 응답한다.', (done)=>{
+        it('id가 숫자가 아닐경우 400으로 응답한다.', done=>{
             request(app)
                 .delete('/users/one')
                 .expect(400)
--- index.js
+++ index.js
@@ -4,17 +4,10 @@ const bodyParser= require('body-parser');
 const app = express();
 const user = require('./api/user');
 
-if(process.env.NODE_ENV !== 'test'){
-    app.use(morgan('dev'));
-}
-app.use(morgan('dev'));
+if(process.env.NODE_ENV !== 'test') app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.use('/users/', user);
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+app.use('/users', user);
 
 module.exports = app;
