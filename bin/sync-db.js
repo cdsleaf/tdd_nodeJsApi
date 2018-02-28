fcc613079ddb244cd640b7bc4422c78b12fbbe51
daesang.cho <daesang.cho@samsung.com>
2018-02-28 13:43:57 +0900

    changed db-sync option

--- bin/sync-db.js
+++ bin/sync-db.js
@@ -1,3 +1,6 @@
 const models = require('../models');
 
-module.exports=()=>models.sequelize.sync({force: true});
+module.exports=()=>{
+    const options = { force: process.env.NODE_ENV === 'test' ? true : false }
+    return models.sequelize.sync(options);
+}
